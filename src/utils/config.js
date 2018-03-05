f7e855d535d7f70065ba4061eb547b610d4cfc52
jimliuxinghai <jimliuxinghai@163.com>
2018-03-05 21:18:24 +0800

    config

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,18 +1,18 @@
 const currentEnv = process.env.NODE_ENV === 'production' ? 'prod' : 'dev'
 
 const env = {
-    // dev: {
-    //     host: 'https://www.mwwae.cn/hotel',
-    //     appid: 'wxdf0a4eff9ba24e02',
-    //     mctCode: 0,
-    //     payUrl: `http://shop0.h.nicelife-china.com`
-    // },
     dev: {
         host: 'https://www.mwwae.cn/hotel',
-        appid: 'wx36d0488dc9e41578',
+        appid: 'wxdf0a4eff9ba24e02',
         mctCode: 0,
         payUrl: `http://shop0.h.nicelife-china.com`
     },
+    // dev: {
+    //     host: 'https://www.mwwae.cn/hotel',
+    //     appid: 'wx36d0488dc9e41578',
+    //     mctCode: 0,
+    //     payUrl: `http://shop0.h.nicelife-china.com`
+    // },
     prod: {
         host: '23',
         appid: '4546',
