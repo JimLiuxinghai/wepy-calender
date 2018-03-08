1b591bb5ec277caa4582122d810392879c65ed08
jimliuxinghai <jimliuxinghai@163.com>
2018-03-08 15:32:02 +0800

    'fix-pay'

--- src/utils/config.js
+++ src/utils/config.js
@@ -5,14 +5,14 @@ const env = {
         host: 'https://www.mwwae.cn/hotel',
         appid: 'wxdf0a4eff9ba24e02',
         mctCode: 0,
-        payUrl: `http://shop0.h.nicelife-china.com`,
+        payUrl: `https://www.mwwae.cn/hotel/miniCallback/wxdf0a4eff9ba24e02`,
         imgUrl: 'http://img.nicelife-china.com:80/i15/'
     },
     prod: {
         host: 'https://shop1.aiduminsu.com/hotel',
         appid: 'wx36d0488dc9e41578',
         mctCode: 0,
-        payUrl: `http://shop0.h.aiduminsu.com`,
+        payUrl: `https://shop1.aiduminsu.com/hotel/miniCallback/wx36d0488dc9e41578`,
         imgUrl: 'http://img.nicelife-china.com:80/i15/'
         //imgUrl: 'http://img.aiduminsu.com:80/i15/'
     }
