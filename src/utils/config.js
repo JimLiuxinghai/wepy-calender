9335b203509651db4b1ab139cdcd78ddf90c3475
jimliuxinghai <jimliuxinghai@163.com>
2018-05-30 23:00:36 +0800

    fix

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,20 +1,20 @@
 const currentEnv = process.env.NODE_ENV === 'production' ? 'prod' : 'dev'
 
 const env = {
-    // dev: {
-    //     host: 'https://www.mwwae.cn/hotel',
-    //     appid: 'wxdf0a4eff9ba24e02',
-    //     mctCode: 0,
-    //     payUrl: `https://www.mwwae.cn/hotel/miniCallback/wxdf0a4eff9ba24e02`,
-    //     imgUrl: 'http://img.nicelife-china.com:80/i15/'
-    // },
     dev: {
-        host: 'https://shop1.aiduminsu.com/hotel',
-        appid: 'wx36d0488dc9e41578',
+        host: 'https://www.mwwae.cn/hotel',
+        appid: 'wxdf0a4eff9ba24e02',
         mctCode: 1111,
-        payUrl: `https://shop1.aiduminsu.com/hotel/miniCallback/wx36d0488dc9e41578`,
-        imgUrl: 'http://img.aiduminsu.com:80/i15/'
+        payUrl: `https://www.mwwae.cn/hotel/miniCallback/wxdf0a4eff9ba24e02`,
+        imgUrl: 'http://img.hotel.mwwae.top/i15/'
     },
+    // dev: {
+    //     host: 'https://shop1.aiduminsu.com/hotel',
+    //     appid: 'wx36d0488dc9e41578',
+    //     mctCode: 1111,
+    //     payUrl: `https://shop1.aiduminsu.com/hotel/miniCallback/wx36d0488dc9e41578`,
+    //     imgUrl: 'http://img.aiduminsu.com:80/i15/'
+    // },
     prod: {
         host: 'https://shop1.aiduminsu.com/hotel',
         appid: 'wx36d0488dc9e41578',
