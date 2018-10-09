e2a026c199ce23a3b5325bbf67dc01f635f4e975
jimliuxinghai <jimliuxinghai@163.com>
2018-10-09 21:35:40 +0800

    fix bug

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,12 +1,17 @@
-const currentEnv = process.env.NODE_ENV === 'production' ? 'prod' : 'dev'
-
+let currentEnv = 'dev'
+if(process.env.NODE_ENV.match('production')) {
+    currentEnv = process.env.NODE_ENV
+}
+else {
+    currentEnv = 'dev'
+}
 const env = {
     dev: {
-        host: 'https://www.mwwae.cn/hotel',
+         host: 'https://shop1.aiduminsu.com/hotel',
         appid: 'wxdf0a4eff9ba24e02',
-        mctCode: 1111,
-        payUrl: `https://www.mwwae.cn/hotel/miniCallback/wxdf0a4eff9ba24e02`,
-        imgUrl: 'http://imgtest.zhongguohanyuan.com/i15/',
+        mctCode: '0000',
+        payUrl: `https://shop1.aiduminsu.com/hotel/miniCallback/wxdf0a4eff9ba24e02`,
+        imgUrl: 'http://img.aiduminsu.com:80/i15/',
         noneedId: true
     },
     // dev: {
@@ -16,14 +21,21 @@ const env = {
     //     payUrl: `https://shop1.aiduminsu.com/hotel/miniCallback/wx36d0488dc9e41578`,
     //     imgUrl: 'http://img.aiduminsu.com:80/i15/'
     // },
-    prod: {
+    production: {
         host: 'https://shop1.aiduminsu.com/hotel',
         appid: 'wx36d0488dc9e41578',
         mctCode: 1111,
         payUrl: `https://shop1.aiduminsu.com/hotel/miniCallback/wx36d0488dc9e41578`,
         imgUrl: 'http://img.aiduminsu.com:80/i15/',
         noneedId: false
+    },
+    productiondf: {
+        host: 'https://shop1.aiduminsu.com/hotel',
+        appid: 'wxdf0a4eff9ba24e02',
+        mctCode: '0000',
+        payUrl: `https://shop1.aiduminsu.com/hotel/miniCallback/wxdf0a4eff9ba24e02`,
+        imgUrl: 'http://img.aiduminsu.com:80/i15/',
+        noneedId: true
     }
 }
-
 module.exports = env[currentEnv]
--- wepy.config.js
+++ wepy.config.js
@@ -1,5 +1,5 @@
 const path = require('path');
-var prod = process.env.NODE_ENV === 'production'
+var prod = process.env.NODE_ENV.match('production')
 
 module.exports = {
   wpyExt: '.wpy',
